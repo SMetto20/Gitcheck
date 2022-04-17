a132b2c533fe08a271a1701af42da5b97bada434
smetto20 <sandra.metto@student.moringaschool.com>
2022-04-17 09:59:36 +0300

    ft-adds forms Module and HttpclientModule to main module

--- src/app/service/githubservice.service.ts
+++ src/app/service/githubservice.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import{HttpClient} from '@angular/common/http'
+import { observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubserviceService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  getInfo(username:string){
+     
+    // const url2 = "https://api.github.com/users/SMetto20/repos"
+    return this.http.get(environment.userUrl + "/" + username + "?"+ environment.apiToken) 
+   }
+ getRepo(username:string){
+   return this.http.get(environment.userUrl + "/" + username + "/repos?"+ environment.apiToken) 
+ }
 }
